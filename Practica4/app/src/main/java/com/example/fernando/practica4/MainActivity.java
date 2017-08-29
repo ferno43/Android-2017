5dadd4a1f66d30b6ea6907fbf5f22d0bfb17e63b
ferno43 <fernalop_12@hotmail.com>
2017-08-29 08:42:14 -0600

    Puse toast

--- Practica4/app/src/main/java/com/example/fernando/practica4/MainActivity.java
+++ Practica4/app/src/main/java/com/example/fernando/practica4/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.fernando.practica4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+        Toast toast=Toast.makeText(this,"cadena", Toast.LENGTH_LONG);
+        toast.show();
         setContentView(R.layout.activity_main);
     }
 }
