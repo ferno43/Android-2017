470d2e7d0141678a3265f20917bd3f1302cf2106
ferno43 <fernalop_12@hotmail.com>
2017-11-07 09:06:00 -0700

    Practica de notificaciones

---
+++ Practicanotificaciones/app/src/main/java/com/example/fernando/practicanotificaciones/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.fernando.practicanotificaciones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("token", FirebaseInstanceId.getInstance().getToken());
+    }
+}
