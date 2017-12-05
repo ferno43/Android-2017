aa37795b0246618a7bbfa0f844002b17379e9c37
ferno43 <fernalop_12@hotmail.com>
2017-12-04 18:57:39 -0700

    Mediquery medio optimizado

--- FinalMoviles1/app/src/main/java/com/example/fernando/finalmoviles1/MainActivity.java
+++ FinalMoviles1/app/src/main/java/com/example/fernando/finalmoviles1/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
                 lista.setVisibility(View.VISIBLE);
                 String especialidad = (String) spinnerEsp.getSelectedItem();
                 JsonURL="http://fernandolopez1.esy.es/slimapi/public/query10/"+especialidad;
-                if(pos!=0){
+                //if(pos!=0){
                 spinnerHosp.setSelection(0);
 
 
@@ -119,7 +119,8 @@ public class MainActivity extends AppCompatActivity {
                 requestQueue.add(obreq);
 
 
-            }}
+            //}
+        }
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
@@ -129,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
                 lista.setVisibility(View.VISIBLE);
                 String hospital = (String) spinnerHosp.getSelectedItem();
                 JsonURL="http://fernandolopez1.esy.es/slimapi/public/query11/"+hospital;
-                if(pos!=0){
+                //if(pos!=0){
                     spinnerEsp.setSelection(0);
                 //Toast.makeText(getApplicationContext(), JsonURL, Toast.LENGTH_SHORT).show();
 
@@ -203,7 +204,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-            }}
+            //}
+            }
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
